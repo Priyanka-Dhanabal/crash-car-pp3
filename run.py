6cd4ce098ad81413dd5fa85d023c3858e48e5ec5
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-27 19:56:45 +0000

    updated car list and created 2 functions, to get a random word and requesting a letter from user

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+import random
+from car_brand import CAR_BRAND
+
 def initial_game_rules():
     '''
     Requesting user to provide input whether 
@@ -21,5 +24,35 @@ def initial_game_rules():
         else:
             print("Invalid choice: Please enter 'Y' or 'N'.\n")
 
+def choose_random_word(list_of_words):
+    '''
+    Selects random word from the list provided.
+    '''
+    return random.choice(list_of_words).upper()
 
-initial_game_rules()
+def get_signle_letter():
+    '''
+    checks if the user input is valid,
+    one letter and a alphabet is provided
+    '''
+    allowed_letter = 'abcdefghijklmnopqrstuvwxyz-'
+    while True:
+        letter = input("Please enter a letter: \n")
+        if letter in allowed_letter and len(letter) == 1:
+            break
+        else:
+            print(f"Only one letter is allowed to be entered and it must be an alphabet or '-'.\nYou entered {letter}. Try again!! \n")
+        
+    return letter
+
+def display_secret_word(word, letters_guessed):
+    '''
+    Shows the initial state of the word.
+    unknown letter are shown as underscore and guessed letter will be displayed.
+    '''
+    secret_word = ''
+    
+        
+
+
+get_signle_letter()
