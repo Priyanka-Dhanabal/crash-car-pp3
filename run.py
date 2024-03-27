ebffdd29e2a88a5d5c16431a0bd8609ddf72c4dd
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-27 21:11:12 +0000

    created a function to play the game

--- run.py
+++ run.py
@@ -32,13 +32,14 @@ def choose_random_word(list_of_words):
 
 def get_single_letter():
     '''
-    checks if the user input is valid,
-    one letter and a alphabet is provided
+    requests and checks if the user input is valid,
+    one letter and if alphabet is provided
     '''
-    allowed_letter = 'abcdefghijklmnopqrstuvwxyz-'
+    allowed_letter = 'ABCDEFGHIJKLMNOPQRSTUVWXZY-'
     while True:
-        letter = input("Please enter a letter: \n")
+        letter = input("Please enter a letter: \n").upper()
         if letter in allowed_letter and len(letter) == 1:
+            print(f"You have entered: {letter}")
             break
         else:
             print(f"Only one letter is allowed to be entered and it must be an alphabet or '-'.\nYou entered {letter}. Try again!! \n")
@@ -59,5 +60,54 @@ def display_secret_word(word, letters_guessed):
     
     return secret_word.upper()
 
+def letters_guessed_update(letters_guessed, letter):
+    """
+    This functions add two args the guess that the player guessed so far
+    to the set of guessed letters.
+    """
+    letters_guessed.add(letter)
 
-print(display_secret_word("ford","f"))
+
+def start_game(word):
+    '''
+    this function manages the game, updates the word status
+    and provide required feedback for bthe user
+    '''
+
+    attempts = 7
+    guess = False
+    letters_guessed = set()
+
+    print(display_secret_word(word, letters_guessed))
+
+    while not guess and attempts > 0:
+        letter = get_single_letter()
+        if letter in letters_guessed:
+            print(f"You already guessed the letter before. Check out the letters guessed: {letters_guessed}")
+        elif letter in random_word:
+            print(f"Great!!! {letter} that is in the word.")
+        else:
+            print(f"{letter} is not in the word. TRY AGAIN!\n")
+            attempts -= 1
+            
+        letters_guessed_update(letters_guessed, letter)
+
+        secret_word = display_secret_word(word, letters_guessed)
+        print(f"You have {attempts} attempts left.\n")
+        print(secret_word)
+
+        print(f"Guessed letters are: {', '.join(letters_guessed)}")
+
+        if word == secret_word:
+            guess = True
+        
+    if guess == True:
+        print("\nYOU WON..!\n")
+    else:
+        print(f"\nYOU LOSE..! The word was {word}\n")   
+
+
+random_word = choose_random_word(CAR_BRAND)
+game = start_game(random_word)
+
+print(game)
