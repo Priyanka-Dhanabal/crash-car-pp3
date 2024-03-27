76db5d074db265f75f518b132060607eb7ea2683
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-03-27 15:20:04 +0000

    created a welcome.py file for banner and game-rules function in run.py file.

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def initial_game_rules():
+    '''
+    Requesting user to provide input whether 
+    user wants to read the game rules.
+    '''
+
+    while True:
+        user_input = input("Do you want to read the rules for this game? (Y/N) \n").lower()
+        if user_input == 'y':
+            print("rules\n")
+            return True
+
+        elif user_input == 'n':
+            print("Lets Start!\n")
+            return False
+        
+        else:
+            print("Invalid choice: Please enter 'Y' or 'N'.\n")
+
+
+initial_game_rules()
